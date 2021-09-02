1afdb440a845f986f3907b11ebaf67923c6eb1ea
Meed Tsao <meed.tsao@esi-tech.net>
2021-09-02 10:39:58 +0800

    add:index.cshtm、Categories.csl 第一個小功能:改成使用 HtmlHelper ，第一個是下拉選單，第二個是金額，第三個是日期，第四個是文字區塊。

---
+++ Homework.SkillTree.MVC/Models/Categories.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Homework.SkillTree.Models
+{
+    public enum CategoryEnum
+    {
+        [Display(Name = "支出")]
+        Expend,
+        [Display(Name = "收入")]
+        Income
+    }
+    public class Categories
+    {
+        [Display(Name ="類別")]
+        public CategoryEnum Category { get; set; }
+        [Display(Name = "金額")]
+        public int Money { get; set; }
+        [Display(Name = "日期")]
+        public DateTime Date { get; set; }
+        [Display(Name = "備註")]
+        public string Notes { get; set; }
+    }
+}
